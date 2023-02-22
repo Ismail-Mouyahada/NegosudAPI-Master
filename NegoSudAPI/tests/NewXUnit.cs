c11ed646997b8a78e70cf0509c1edca31ebd1504
ISMAIL MOUYAHADA <66369128+Ismail-Mouyahada@users.noreply.github.com>
2023-02-22 11:56:14 +0100

    updates

---
+++ NegoSudAPI/tests/NewXUnit.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NegoSudAPI.tests
+{
+    public class NewXUnit
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
